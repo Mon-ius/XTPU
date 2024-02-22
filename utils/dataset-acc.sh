171ca1f463c3b32ce04ff1334cc1b01d6fff5837
Monius <m1105930979@gmail.com>
2024-02-23 06:27:35 +0800

    add dataset acceleration

---
+++ utils/dataset-acc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get -qq update && sudo apt-get -qq dist-upgrade \
+    && sudo apt-get -qq install curl net-tools tmux rclone \
+    && sudo apt-get -qq autoremove --purge && sudo apt-get clean
+
+_name="$1"
+_id="$2"
+_secret="$3"
+_drive="$4"
+_token="$5"
+
+rclone config create "$_name" onedrive client_id "$_id" client_secret "$_secret" config_refresh_token false token "$_token" drive_id "$_drive" drive_type business 
+
+rclone sync -P dataset:/"$_name" /dev/shm/"$_name" --transfers 8 --fast-list --progress --tpslimit 48 --drive-chunk-size 512M --max-transfer 2000G --multi-thread-streams 24 --onedrive-chunk-size 128000k --sharefile-chunk-size 512M
--- utils/debian-acc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -qq update && sudo apt-get -qq dist-upgrade \
-    && sudo apt-get -qq install curl net-tools tmux rclone \
-    && sudo apt-get -qq autoremove --purge && sudo apt-get clean
