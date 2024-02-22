568a68f4806cf0e734c0ec1a50097a86606f35f3
Monius <m1105930979@gmail.com>
2024-02-23 06:44:15 +0800

    update token

--- utils/dataset-acc.sh
+++ utils/dataset-acc.sh
@@ -8,8 +8,9 @@ _name="$1"
 _id="$2"
 _secret="$3"
 _drive="$4"
-_token="$5"
 
-rclone config create "$_name" onedrive client_id "$_id" client_secret "$_secret" config_refresh_token false token "$_token" drive_id "$_drive" drive_type business 
+token=$(rclone authorize "onedrive" "$_id" "$_secret" --auth-no-open-browser)
+
+rclone config create "$_name" onedrive client_id "$_id" client_secret "$_secret" config_refresh_token false token "$token" drive_id "$_drive" drive_type business 
 
 rclone sync -P dataset:/"$_name" /dev/shm/"$_name" --transfers 8 --fast-list --progress --tpslimit 48 --drive-chunk-size 512M --max-transfer 2000G --multi-thread-streams 24 --onedrive-chunk-size 128000k --sharefile-chunk-size 512M
