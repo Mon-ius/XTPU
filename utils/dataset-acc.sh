d8d9317e261a83b8806271e17cfa9a442e94043f
Monius <m1105930979@gmail.com>
2024-02-29 01:51:01 +0800

    add mosh

--- utils/dataset-acc.sh
+++ utils/dataset-acc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get -qq update && sudo apt-get -qq dist-upgrade \
-    && sudo apt-get -qq install curl net-tools tmux rclone \
+    && sudo apt-get -qq install curl net-tools tmux rclone mosh \
     && sudo apt-get -qq autoremove --purge && sudo apt-get clean
 
 l_name='mount'
