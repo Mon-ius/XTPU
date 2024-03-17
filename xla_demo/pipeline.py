e44b4d0902455841acea7f720c2f10090241e5a0
Monius <m1105930979@gmail.com>
2024-03-17 20:04:04 +0800

    add more usage

--- xla_demo/pipeline.py
+++ xla_demo/pipeline.py
@@ -1,8 +1,12 @@
-import torch
 from transformers import pipeline
+import torch
 
+import torch_xla.core.xla_model as xm
+
+device = xm.xla_device()
 
 llm = "microsoft/phi-2"
-generator = pipeline("text-generation", model=llm, device_map="auto", torch_dtype=torch.bfloat16)
+pipe = pipeline("text-generation", model=llm, torch_dtype=torch.bfloat16, device=device)
+text = pipe("More and more large language models are opensourced so Hugging Face has", max_length=1024)
 
-generator("More and more large language models are opensourced so Hugging Face has")
+print(text)
