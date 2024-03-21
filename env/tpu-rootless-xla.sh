d24725fa56b5edfc2c6e89345de5fd810f4d8a24
Monius <m1105930979@gmail.com>
2024-03-22 00:37:55 +0800

    add open3d

--- env/tpu-rootless-xla.sh
+++ env/tpu-rootless-xla.sh
@@ -66,7 +66,7 @@ bash $DEV_ROOT/conda.sh -b -p $CONDA_ROOT_PREFIX && rm $DEV_ROOT/conda.sh
 
 . "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh"
 
-conda create -n xla python=3.11 datasets accelerate evaluate scikit-learn torchvision torchaudio transformers bitsandbytes diffusers segment-anything sentencepiece imageio scipy numpy pyglet trimesh gradio fire -c conda-forge -c pytorch -y
+conda create -n xla python=3.11 datasets accelerate evaluate scikit-learn torchvision torchaudio transformers bitsandbytes diffusers segment-anything sentencepiece imageio scipy numpy pyglet trimesh open3d gradio fire -c conda-forge -c pytorch -y
 conda activate xla
 conda env config vars set LD_LIBRARY_PATH="$CONDA_PREFIX/lib"
 conda env config vars set HF_HOME="/dev/shm"
