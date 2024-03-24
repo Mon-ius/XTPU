a72625e585d3fbd9bcb3aafc1b07a296cafe5127
Monius <m1105930979@gmail.com>
2024-03-25 02:12:03 +0800

    add cuda

--- env/cuda/v2.2.sh
+++ env/cuda/v2.2.sh
@@ -8,6 +8,10 @@ conda env config vars set HF_HOME=/dev/shm
 conda env config vars set PJRT_DEVICE=CUDA
 conda env config vars set GPU_NUM_DEVICES=4
 
+conda deactivate
+sleep 5
+conda activate x3
+
 pip install torch torchvision torchaudio sentencepiece
 pip install https://storage.googleapis.com/pytorch-xla-releases/wheels/cuda/12.1/torch_xla-2.2.0-cp310-cp310-manylinux_2_28_x86_64.whl
 pip install git+https://github.com/huggingface/accelerate
@@ -28,5 +32,7 @@ t1 = torch.randn(3,3,device=dev)
 t2 = torch.randn(3,3,device=dev)
 print(t1 + t2)' | tee /tmp/run.py
 PJRT_DEVICE=CUDA GPU_NUM_DEVICES=4 python /tmp/run.py
+
+rm -rf /tmp/xla 
 git clone -j8 --depth 1 --branch main https://github.com/pytorch/xla.git /tmp/xla
 PJRT_DEVICE=CUDA GPU_NUM_DEVICES=4 python /tmp/xla/test/test_train_mp_imagenet.py --fake_data
--- env/cuda/v2.3_failed.sh
+++ env/cuda/v2.3_failed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -n x3 python=3.10 cuda -c nvidia -y
+conda create -n x3 python=3.11 cuda -c nvidia -y
 conda activate x3
 
 conda env config vars set LD_LIBRARY_PATH="$CONDA_PREFIX/lib"
@@ -8,8 +8,12 @@ conda env config vars set HF_HOME=/dev/shm
 conda env config vars set PJRT_DEVICE=CUDA
 conda env config vars set GPU_NUM_DEVICES=4
 
-pip install torch==2.3.0.dev20240315+cu121 torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
-pip install https://storage.googleapis.com/pytorch-xla-releases/wheels/cuda/12.1/torch_xla-2.3.0rc2-cp310-cp310-linux_x86_64.whl
+conda deactivate
+sleep 5
+conda activate x3
+
+pip install torch torchvision torchaudio sentencepiece
+pip install https://storage.googleapis.com/pytorch-xla-releases/wheels/cuda/12.1/torch_xla-nightly-cp311-cp311-linux_x86_64.whl
 
 python -c "import torch; print(torch.__version__, torch.version.cuda);"
 python -c "import torch_xla; print(torch_xla.__version__);"
@@ -28,5 +32,7 @@ t2 = torch.randn(3,3,device=dev)
 print(t1 + t2)' | tee /tmp/run.py
 
 PJRT_DEVICE=CUDA GPU_NUM_DEVICES=4 python /tmp/run.py
-git clone -j8 --depth 1 --branch main https://github.com/pytorch/xla.git
-PJRT_DEVICE=CUDA GPU_NUM_DEVICES=4 python xla/test/test_train_mp_imagenet.py --fake_data
+
+rm -rf /tmp/xla 
+git clone -j8 --depth 1 --branch main https://github.com/pytorch/xla.git /tmp/xla
+PJRT_DEVICE=CUDA GPU_NUM_DEVICES=4 python /tmp/xla/test/test_train_mp_imagenet.py --fake_data
