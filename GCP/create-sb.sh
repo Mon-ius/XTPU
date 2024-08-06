bc27f0fb35936dda13ca247ff74ee103c40354a6
Monius <m1105930979@gmail.com>
2024-08-06 12:46:15 +0800

    add sb

---
+++ GCP/create-sb.sh
@@ -0,0 +1,62 @@
+#!/bin/dash
+
+SAGER_NET="https://sing-box.app/gpg.key"
+curl -fsSL "$SAGER_NET" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sagernet.gpg
+echo "deb https://deb.sagernet.org * *" | sudo tee /etc/apt/sources.list.d/sagernet.list
+sudo apt-get update && sudo apt-get install sing-box
+
+_CF_ZONE="sub"
+_CF_DOMAIN="example.com"
+_CF_TOKEN="jdqgyu2g3u1309i09i0"
+
+CF_TOKEN="${1:-$_CF_TOKEN}"
+CF_DOMAIN="${2:-$_CF_DOMAIN}"
+CF_ZONE="${3:-$_CF_ZONE}"
+
+curl -fsSL bit.ly/new-gcp-dns | sh -s -- "$CF_TOKEN" "$CF_DOMAIN" "$CF_ZONE"
+
+cat <<EOF | sudo tee /etc/sing-box/config.json
+{
+    "outbounds": [
+        {
+            "type": "direct",
+            "tag": "direct"
+        }
+    ],
+    "inbounds": [
+        {   
+            "sniff": true,
+            "sniff_override_destination": true,
+            "type": "hysteria2",
+            "listen": "::",
+            "listen_port": 443,
+            "up_mbps": 1000,
+            "down_mbps": 1000,
+            "users": [
+                {
+                    "password": "$CF_TOKEN"
+                }
+            ],
+            "tls": {
+                "enabled": true,
+                "server_name": "$CF_ZONE.$CF_DOMAIN",
+                "acme": {
+                    "domain": "$CF_ZONE.$CF_DOMAIN",
+                    "email": "admin@$CF_DOMAIN",
+                    "dns01_challenge": {
+                        "provider": "cloudflare",
+                        "api_token": "$CF_TOKEN"
+                    }
+                },
+                "alpn": [
+                    "h3"
+                ]
+            }
+        }
+    ]
+}
+EOF
+
+sudo systemctl enable sing-box
+sudo systemctl daemon-reload
+sudo systemctl restart sing-box
