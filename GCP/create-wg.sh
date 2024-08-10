b359a4c0aa22e874e926d4e8b8d080c7a0fbd140
Monius <m1105930979@gmail.com>
2024-08-10 16:55:51 +0800

    update

--- GCP/create-wg.sh
+++ GCP/create-wg.sh
@@ -1,15 +1,13 @@
 #!/bin/dash
 
-END_POINT="https://api.cloudflareclient.com/v0a2077/reg"
-API_VERSION="a-7.21-0721"
+END_POINT="https://api.cloudflareclient.com/v0a2077"
+REG_URL="$END_POINT/reg"
 
 keypair=$(openssl genpkey -algorithm X25519)
 private_key=$(echo "$keypair" | openssl pkey -outform DER | tail -c 32 | base64)
 public_key=$(echo "$keypair" | openssl pkey -pubout -outform DER | tail -c 32 | base64)
 
-RESPONSE=$(curl -fsSL -X POST $END_POINT \
-    -H "Content-Type: application/json" \
-    -H "CF-Client-Version: $API_VERSION" \
+RESPONSE=$(curl -fsSL $REG_URL \
     -d '{
         "key":"'${public_key}'",
         "tos":"'$(date +"%Y-%m-%dT%H:%M:%S.000Z")'"
