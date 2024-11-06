d9e0db9819e9d25d23e8cc60366804b362fc6d20
Monius <m1105930979@gmail.com>
2024-11-06 14:44:57 +0800

    update

--- GCP/create-ssh-tun.sh
+++ GCP/create-ssh-tun.sh
@@ -1,7 +1,12 @@
 #!/bin/dash
 
-XUSER=tun
-sudo adduser --disabled-password --disabled-login --gecos "" $XUSER
-sudo mkdir -p /home/$XUSER/.ssh
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYDGKmE+0M4PcMH/kTupjmcf/Ic9VbvXKD7fPHNVdem' | sudo tee /home/$XUSER/.ssh/authorized_keys
-sudo chown -R $XUSER:$XUSER /home/$XUSER/.ssh
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+RUSER=$1
+sudo adduser --disabled-password --disabled-login --gecos "" "$RUSER"
+sudo mkdir -p "/home/$RUSER/.ssh"
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYDGKmE+0M4PcMH/kTupjmcf/Ic9VbvXKD7fPHNVdem' | sudo tee "/home/$RUSER/.ssh/authorized_keys"
+sudo chown -R "$RUSER:$RUSER" "/home/$RUSER/.ssh"
