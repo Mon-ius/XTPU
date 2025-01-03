4042fbe611dff11abc0efefee7ec57ba7d6f5f56
Monius <m1105930979@gmail.com>
2025-01-03 15:48:50 +0800

    update

--- utils/debian-rootfs.sh
+++ utils/debian-rootfs.sh
@@ -14,16 +14,23 @@ sudo dd bs=4M status=progress if=/dev/zero of="$_DEV"
 
 sudo sfdisk "$_DEV" <<EOF
 label: gpt
+size=1G, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 size=1G, type=21686148-6449-6E6F-744E-656564454649
-size=2G, type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
-size=, type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
+size=2G, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+size=, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
-sudo mkfs.msdos -F 32 -n 'Boot' /dev/sdb1
+sudo mkfs.msdos -F 32 -n 'Boot' "$_DEV"1
+sudo mkfs.ext4 "$_DEV"2
+sudo mkfs.ext4 "$_DEV"3
 
-sudo mkfs.fat -F32 /dev/sdb1
-sudo mkfs.ext4 /dev/sdb2
-sudo mkfs.ext4 /dev/sdb3
+sudo mkdir -p /boot/EFI/syslinux
+sudo cp -r /usr/lib/syslinux/modules/efi64/* /boot/EFI/syslinux
+sudo cp /usr/share/preloader-signed/PreLoader.efi /boot/EFI/syslinux
+sudo cp /usr/share/preloader-signed/HashTool.efi /boot/EFI/syslinux
+sudo cp /boot/EFI/syslinux/syslinux.efi /boot/EFI/syslinux/loader.efi
+
+sudo efibootmgr -c -d "$_DEV" -p 1 -l \\efi\\syslinux\\syslinux.efi -L Gentoo
 
 sudo mkdir -p "$MNT_ROOT"
 sudo mkdir -p "$MNT_BOOT"
