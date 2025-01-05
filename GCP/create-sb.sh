855dac7109f5bd41663dc00f5092d6b5d4e600f6
Monius <m1105930979@gmail.com>
2025-01-05 23:17:22 +0800

    add warp

--- GCP/create-sb.sh
+++ GCP/create-sb.sh
@@ -10,21 +10,59 @@ sudo apt-get update && sudo apt-get install sing-box
 _CF_ZONE="sub"
 _CF_DOMAIN="example.com"
 _CF_TOKEN="jdqgyu2g3u1309i09i0"
+_WARP_SERVER=engage.cloudflareclient.com
+_WARP_PORT=2408
+_NET_PORT=9091
 
 CF_TOKEN="${1:-$_CF_TOKEN}"
 CF_DOMAIN="${2:-$_CF_DOMAIN}"
 CF_ZONE="${3:-$_CF_ZONE}"
+WARP_SERVER="${WARP_SERVER:-$_WARP_SERVER}"
+WARP_PORT="${WARP_PORT:-$_WARP_PORT}"
+NET_PORT="${NET_PORT:-$_NET_PORT}"
 
 curl -fsSL bit.ly/new-gcp-dns | sh -s -- "$CF_TOKEN" "$CF_DOMAIN" "$CF_ZONE"
 
-cat <<EOF | sudo tee /etc/sing-box/config.json
-{
-    "outbounds": [
+RESPONSE=$(curl -fsSL bit.ly/warp_socks | sh)
+private_key=$(echo "$RESPONSE" | sed -n 's/.*"private_key":"\([^"]*\)".*/\1/p')
+ipv4=$(echo "$RESPONSE" | sed -n 's/.*"v4":"\([^"]*\)".*/\1/p')
+ipv6=$(echo "$RESPONSE" | sed -n 's/.*"v6":"\([^"]*\)".*/\1/p')
+public_key=$(echo "$RESPONSE" | sed -n 's/.*"public_key":"\([^"]*\)".*/\1/p')
+client_hex=$(echo "$RESPONSE" | grep -o '"client_id":"[^"]*' | cut -d'"' -f4 | base64 -d | od -t x1 -An | tr -d ' \n')
+reserved_dec=$(echo "$client_hex" | awk '{printf "[%d, %d, %d]", "0x"substr($0,1,2), "0x"substr($0,3,2), "0x"substr($0,5,2)}')
+
+WARP_PART=$(cat <<EOF
+        {
+            "tag": "WARP",
+            "type": "wireguard",
+            "server": "$WARP_SERVER",
+            "server_port": $WARP_PORT,
+            "local_address": [
+                "${ipv4}/32",
+                "${ipv6}/128"
+            ],
+            "private_key": "$private_key",
+            "peer_public_key": "$public_key",
+            "reserved": $reserved_dec,
+            "mtu": 1408,
+            "udp_fragment": true
+        }
+EOF
+)
+
+DIRECT_PART=$(cat <<EOF
         {
             "tag": "direct-out",
             "udp_fragment": true,
             "type": "direct"
         }
+EOF
+)
+
+cat <<EOF | sudo tee /etc/sing-box/config.json
+{
+    "outbounds": [
+$WARP_PART
     ],
     "inbounds": [
         {   
