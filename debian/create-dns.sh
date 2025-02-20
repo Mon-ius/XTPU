bd2f055fac2ed63e23e0afe9deef74384ce843da
Monius <m1105930979@gmail.com>
2025-02-20 19:46:37 +0800

    update

--- debian/create-dns.sh
+++ debian/create-dns.sh
@@ -5,3 +5,12 @@ sudo tee /etc/resolv.conf > /dev/null << 'EOF'
 nameserver 1.1.1.1
 search .
 EOF
+
+sudo chattr +i /etc/resolv.conf
+sudo tee /etc/dpkg/dpkg.cfg.d/resolv-conf-protect > /dev/null << 'EOF'
+path-exclude=/etc/resolv.conf
+EOF
+
+sudo tee /etc/apt/apt.conf.d/100-resolv-conf-protect > /dev/null << 'EOF'
+DPkg::Post-Invoke {"chattr +i /etc/resolv.conf || true";};
+EOF
