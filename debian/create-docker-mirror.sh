ec2313c7168d9ab1b6e9ca990eeaa106f1a14440
Monius <m1105930979@gmail.com>
2025-02-21 00:09:10 +0800

    update

--- debian/create-docker-mirror.sh
+++ debian/create-docker-mirror.sh
@@ -9,7 +9,7 @@ curl -fsSL "$DOCKER" | sudo -E gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/doc
 echo "deb [arch=$ARCH] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $VER stable" | sudo -E tee /etc/apt/sources.list.d/docker.list
 
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y docker-ce docker-compose-plugin
+sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y docker-ce docker-compose-plugin docker-compose
 
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<EOF
--- debian/create-docker.sh
+++ debian/create-docker.sh
@@ -12,7 +12,7 @@ curl -fsSL "$DOCKER" | sudo -E gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/doc
 echo "deb [arch=$ARCH] https://download.docker.com/linux/debian $VER stable" | sudo -E tee /etc/apt/sources.list.d/docker.list
 
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y docker-ce docker-compose-plugin
+sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y docker-ce docker-compose-plugin docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
