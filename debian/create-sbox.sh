78311591489e0c2eb40eaafa458a6751642d88d6
Monius <m1105930979@gmail.com>
2025-02-25 04:37:31 +0800

    update

--- debian/create-sbox.sh
+++ debian/create-sbox.sh
@@ -9,12 +9,11 @@ curl -fsSL "$SAGER_NET" | sudo -E gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/
 echo "deb https://deb.sagernet.org * *" | sudo -E tee /etc/apt/sources.list.d/sagernet.list
 
 sudo -E apt-get -qq update
-is_reboot=$(apt list --upgradable 2>/dev/null | grep -q "^sing-box/" && echo true || echo false)
-
 if ! dpkg -l "sing-box" 2>/dev/null | grep -q '^ii'; then
     sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y sing-box
 fi
 
+is_reboot=$(apt list --upgradable 2>/dev/null | grep -q "^sing-box/" && echo true || echo false)
 if [ "$is_reboot" = true ]; then
     sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y sing-box
     sudo reboot
