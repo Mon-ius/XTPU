5812e28026fbcf7aff7532e2fae24d61a88b5c31
Monius <m1105930979@gmail.com>
2025-02-25 04:28:57 +0800

    update

--- debian/create-warp.sh
+++ debian/create-warp.sh
@@ -13,7 +13,7 @@ RESPONSE=$(curl -sX POST "$WARP_API" \
     -H 'user-agent: okhttp/4.12.1' \
     -d '{
         "tos": "'"$(date --utc +"%Y-%m-%dT%H:%M:%S.%3NZ" | awk '{print substr($0, 1, length($0)-1)"-02:00"}')"'",
-        "key": "'$PUB'"
+        "key": "'"$PUB"'"
     }')
 
 ipv4=$(echo "$RESPONSE" | sed -n 's/.*"v4":"\([^"]*\)".*/\1/p')
@@ -51,102 +51,104 @@ WARP_PART=$(cat <<EOF
 EOF
 )
 
-sudo tee /etc/sing-box/config.json > /dev/null << EOF
-{
-    "log": {
-        "disabled": false,
-        "level": "debug",
-        "timestamp": true
-    },
-    "experimental": {
-        "cache_file": {
-            "enabled": true,
-            "path": "cache.db",
-            "cache_id": "v1"
-        }
-    },
-    "dns": {
-        "servers": [
-            {
-                "tag": "ND-h3",
-                "address": "h3://dns.nextdns.io/x",
-                "address_resolver": "dns-direct",
-                "detour": "direct-out"
-            },
-            {
-                "tag": "dns-direct",
-                "address": "udp://223.5.5.5",
-                "detour": "direct-out"
-            }
-        ],
-        "strategy": "ipv4_only",
-        "final": "ND-h3",
-        "reverse_mapping": true,
-        "disable_cache": false,
-        "disable_expire": false
-    },
-    "route": {
-        "rules": [
-            {
-                "inbound": "mixed-in",
-                "action": "sniff",
-                "sniffer": [
-                    "dns",
-                    "bittorrent",
-                    "http",
-                    "tls",
-                    "quic",
-                    "dtls"
-                ]
-            },
-            {
-                "protocol": "dns",
-                "action": "hijack-dns"
-            },
-            {
-                "ip_is_private": true,
-                "action": "route",
-                "outbound": "direct-out"
-            },
-            {
-                "ip_cidr": [
-                    "0.0.0.0/8",
-                    "10.0.0.0/8",
-                    "127.0.0.0/8",
-                    "169.254.0.0/16",
-                    "172.16.0.0/12",
-                    "192.168.0.0/16",
-                    "224.0.0.0/4",
-                    "240.0.0.0/4",
-                    "52.80.0.0/16"
-                ],
-                "action": "route",
-                "outbound": "direct-out"
-            }
-        ],
-        "auto_detect_interface": true,
-        "final": "WARP"
-    },
-    "inbounds": [
-        {
-            "type": "mixed",
-            "tag": "mixed-in",
-            "listen": "::",
-            "listen_port": 9091
-        }
-    ],
-$WARP_PART,
-    "outbounds": [
-        {
-            "tag": "direct-out",
-            "type": "direct",
-            "udp_fragment": true
-        }
-    ]
-}
-EOF
+echo "$WARP_PART"
 
-sudo systemctl restart sing-box
-sudo systemctl stop sing-box
-sudo sing-box -c /etc/sing-box/config.json run
-curl -x "socks5h://127.0.0.1:9091" -fsSL "https://www.cloudflare.com/cdn-cgi/trace"
+# sudo tee /etc/sing-box/config.json > /dev/null << EOF
+# {
+#     "log": {
+#         "disabled": false,
+#         "level": "debug",
+#         "timestamp": true
+#     },
+#     "experimental": {
+#         "cache_file": {
+#             "enabled": true,
+#             "path": "cache.db",
+#             "cache_id": "v1"
+#         }
+#     },
+#     "dns": {
+#         "servers": [
+#             {
+#                 "tag": "ND-h3",
+#                 "address": "h3://dns.nextdns.io/x",
+#                 "address_resolver": "dns-direct",
+#                 "detour": "direct-out"
+#             },
+#             {
+#                 "tag": "dns-direct",
+#                 "address": "udp://223.5.5.5",
+#                 "detour": "direct-out"
+#             }
+#         ],
+#         "strategy": "ipv4_only",
+#         "final": "ND-h3",
+#         "reverse_mapping": true,
+#         "disable_cache": false,
+#         "disable_expire": false
+#     },
+#     "route": {
+#         "rules": [
+#             {
+#                 "inbound": "mixed-in",
+#                 "action": "sniff",
+#                 "sniffer": [
+#                     "dns",
+#                     "bittorrent",
+#                     "http",
+#                     "tls",
+#                     "quic",
+#                     "dtls"
+#                 ]
+#             },
+#             {
+#                 "protocol": "dns",
+#                 "action": "hijack-dns"
+#             },
+#             {
+#                 "ip_is_private": true,
+#                 "action": "route",
+#                 "outbound": "direct-out"
+#             },
+#             {
+#                 "ip_cidr": [
+#                     "0.0.0.0/8",
+#                     "10.0.0.0/8",
+#                     "127.0.0.0/8",
+#                     "169.254.0.0/16",
+#                     "172.16.0.0/12",
+#                     "192.168.0.0/16",
+#                     "224.0.0.0/4",
+#                     "240.0.0.0/4",
+#                     "52.80.0.0/16"
+#                 ],
+#                 "action": "route",
+#                 "outbound": "direct-out"
+#             }
+#         ],
+#         "auto_detect_interface": true,
+#         "final": "WARP"
+#     },
+#     "inbounds": [
+#         {
+#             "type": "mixed",
+#             "tag": "mixed-in",
+#             "listen": "::",
+#             "listen_port": 9091
+#         }
+#     ],
+# $WARP_PART,
+#     "outbounds": [
+#         {
+#             "tag": "direct-out",
+#             "type": "direct",
+#             "udp_fragment": true
+#         }
+#     ]
+# }
+# EOF
+
+# sudo systemctl restart sing-box
+# sudo systemctl stop sing-box
+# sudo sing-box -c /etc/sing-box/config.json run
+# curl -x "socks5h://127.0.0.1:9091" -fsSL "https://www.cloudflare.com/cdn-cgi/trace"
