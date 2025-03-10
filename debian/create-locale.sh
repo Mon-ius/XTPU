802df48957f2f092bfe2cd8ab9956bcba2df7137
Monius <m1105930979@gmail.com>
2025-03-10 20:58:05 +0800

    update

--- debian/create-locale.sh
+++ debian/create-locale.sh
@@ -3,6 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y locales
+sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 
 sudo tee /etc/default/locale > /dev/null << 'EOF'
 LANG="en_US.UTF-8"
@@ -18,6 +19,8 @@ LC_TELEPHONE="en_US.UTF-8"
 LC_MEASUREMENT="en_US.UTF-8"
 LC_IDENTIFICATION="en_US.UTF-8"
 EOF
+
+sudo dpkg-reconfigure --frontend=noninteractive locales
 
 # sudo tee /etc/default/locale > /dev/null << 'EOF'
 # LC_ALL="en_US.UTF-8"
