414d0861cc81d53421e2d035dd4de933897fc3da
Monius <m1105930979@gmail.com>
2025-03-10 22:06:45 +0800

    add go-lang

---
+++ debian/create-golang.sh
@@ -0,0 +1,18 @@
+#!/bin/dash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y tar curl
+
+ARCH=$(dpkg --print-architecture)
+GO_VERSION=$(curl -fsSL "https://go.dev/VERSION?m=text" | head -n 1 | sed 's/go//')
+GOLANG="https://golang.org/dl/go${GO_VERSION}.linux-${ARCH}.tar.gz"
+
+sudo rm -rf /usr/local/go
+curl -sSL "$GOLANG" | sudo tar -xz -C /usr/local
+cat <<'EOF' | tee -a "$HOME/.profile"
+export PATH=$PATH:/usr/local/go/bin
+EOF
+
+export PATH=$PATH:/usr/local/go/bin
