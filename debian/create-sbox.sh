103a9f3fab687ea83e3630a61d52e79ed3f5e7ef
Monius <m1105930979@gmail.com>
2025-03-26 16:37:32 +0800

    update

--- debian/create-sbox.sh
+++ debian/create-sbox.sh
@@ -4,6 +4,11 @@ export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y gnupg2 curl jq
 
+if [ -f "/usr/bin/sing-box-beta" ]; then
+    echo "sing-box-beta found at /usr/bin/sing-box-beta"
+    sudo -E apt-get -qq remove --purge -y sing-box
+fi
+
 SAGER_NET="https://sing-box.app/gpg.key"
 curl -fsSL "$SAGER_NET" | sudo -E gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/sagernet.gpg
 echo "deb https://deb.sagernet.org * *" | sudo -E tee /etc/apt/sources.list.d/sagernet.list
