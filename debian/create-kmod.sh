cd8a95118e744869928c75479b8cc061058a2378
Monius <m1105930979@gmail.com>
2025-04-01 05:51:19 +0800

    update

--- debian/create-kmod.sh
+++ debian/create-kmod.sh
@@ -33,65 +33,31 @@ tcp_bbr
 EOF
 
 sudo tee /etc/sysctl.d/bbr.conf > /dev/null << 'EOF'
+
 net.core.default_qdisc=fq_codel
-net.core.netdev_budget = 600
 net.core.optmem_max = 25165824
-net.core.somaxconn = 3276800
+net.core.wmem_max = 26214400
+net.core.rmem_max = 26214400
+net.core.somaxconn = 65535
 net.core.netdev_max_backlog = 65535
-net.ipv4.tcp_max_syn_backlog = 65535
-net.ipv4.tcp_collapse_max_bytes=6291456
-net.ipv4.tcp_notsent_lowat=131072
-net.ipv4.tcp_abort_on_overflow=1
 net.ipv4.tcp_adv_win_scale=-2
 net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_slow_start_after_idle=1
-net.ipv4.tcp_no_metrics_save=1
-net.ipv4.tcp_mtu_probing = 1
-
-net.ipv4.tcp_syn_retries=3
-net.ipv4.tcp_synack_retries=3
-net.ipv4.tcp_retries2=5
-net.ipv4.tcp_syncookies=0
 net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_moderate_rcvbuf = 1
 net.ipv4.tcp_timestamps = 0
 net.ipv4.tcp_fin_timeout = 15
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.tcp_max_syn_backlog = 65535
 net.ipv4.tcp_max_tw_buckets = 2000000
 net.ipv4.tcp_keepalive_time = 1200
 net.ipv4.tcp_keepalive_intvl = 60
 net.ipv4.tcp_keepalive_probes = 5
-net.ipv4.tcp_congestion_control=bbr
-net.ipv4.tcp_rmem=4096 87380 192000000
-net.ipv4.tcp_wmem=4096 65536 192000000
-net.ipv4.route.gc_timeout=100
-net.unix.max_dgram_qlen=1024
-
-net.nf_conntrack_max=1000000
-net.netfilter.nf_conntrack_max = 1000000
-net.netfilter.nf_conntrack_tcp_timeout_fin_wait=30
-net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
-net.netfilter.nf_conntrack_tcp_timeout_close_wait=15
-net.netfilter.nf_conntrack_tcp_timeout_established=300
-net.ipv4.netfilter.ip_conntrack_tcp_timeout_established=7200
-
+net.netfilter.nf_conntrack_max = 1048576
 fs.file-max = 2097152
 fs.inode-max = 4194304
 fs.nr_open = 1073741816
-
-vm.dirty_ratio=10
-vm.overcommit_memory=1
-vm.panic_on_oom=1
-vm.swappiness=10
-vm.vfs_cache_pressure=250
-vm.zone_reclaim_mode=0
 vm.max_map_count = 524288
-
-kernel.panic=1
-kernel.pid_max=32768
-kernel.shmall=1073741824
-kernel.shmmax=4294967296
-kernel.sysrq=1
 EOF
 
 sudo tee /etc/sysctl.d/99-allow-ping.conf > /dev/null << 'EOF'
