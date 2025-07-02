97f73fe30fa231e12b2961c869d4191343dabd6b
Monius <m1105930979@gmail.com>
2025-07-03 07:21:49 +0800

    update

--- debian/create-api-token.sh
+++ debian/create-api-token.sh
@@ -60,12 +60,16 @@ JSON_PAYLOAD='{
             },
             "permission_groups": [
                 {
-                    "id": "1af1fa2adc104452b74a9a3364202f20",
-                    "name": "Account Settings Write"
-                },
-                {
                     "id": "4755a26eedb94da69e1066d98aa820be",
                     "name": "DNS Write"
+                },
+                {
+                    "id": "c8fed203ed3043cba015a93ad1616f1f",
+                    "name": "Zone Read"
+                },
+                {
+                    "id": "1af1fa2adc104452b74a9a3364202f20",
+                    "name": "Account Settings Write"
                 },
                 {
                     "id": "b4992e1108244f5d8bfbd5744320c2e1",
@@ -76,16 +80,40 @@ JSON_PAYLOAD='{
                     "name": "Workers R2 Storage Write"
                 },
                 {
-                    "id": "6c8a3737f07f46369c1ea1f22138daaf",
-                    "name": "AI Gateway Write"
+                    "id": "45db74139a62490b9b60eb7c4f34994b",
+                    "name": "Workers R2 Data Catalog Read"
+                },
+                {
+                    "id": "d229766a2f7f4d299f20eaa8c9b1fde9",
+                    "name": "Workers R2 Data Catalog Write"
+                },
+                {
+                    "id": "f7f0eda5697f475c90846e879bab8666",
+                    "name": "Workers KV Storage Write"
+                },
+                {
+                    "id": "e086da7e2179491d91ee5f35b3ca210a",
+                    "name": "Workers Scripts Write"
                 },
                 {
                     "id": "bacc64e0f6c34fc0883a1223f938a104",
                     "name": "Workers AI Write"
                 },
                 {
-                    "id": "1af1fa2adc104452b74a9a3364202f20",
-                    "name": "Account Settings Write"
+                    "id": "2e095cf436e2455fa62c9a9c2e18c478",
+                    "name": "Workers CI Write"
+                },
+                {
+                    "id": "bdbcd690c763475a985e8641dddc09f7",
+                    "name": "Workers Containers Write"
+                },
+                {
+                    "id": "28f4b596e7d643029c524985477ae49a",
+                    "name": "Workers Routes Write"
+                },
+                {
+                    "id": "6c8a3737f07f46369c1ea1f22138daaf",
+                    "name": "AI Gateway Write"
                 }
             ]
         }
@@ -105,16 +133,6 @@ echo "[SUCCESS] Token created successfully"
 echo "Token Value: $TOKEN_VALUE"
 echo "Token ID/ACCESS_KEY: $TOKEN_ID"
 echo "Token SECRET_KEY: $SECRET_KEY"
-
-
-R2_ACCESS_KEY=$(echo "$TOKEN_RESPONSE" | grep -o '"id":"[^"]*' | cut -d'"' -f4 | head -n 1)
-R2_TOKEN_VALUE=$(echo "$TOKEN_RESPONSE" | grep -o '"value":"[^"]*' | cut -d'"' -f4)
-R2_SECRET_KEY=$(echo -n "$R2_TOKEN_VALUE" | sha256sum | cut -d' ' -f1)
-
-echo "[SUCCESS] R2 configured"
-echo "Endpoint: $R2_ENDPOINT"
-echo "ACCESS KEY: $R2_ACCESS_KEY"
-echo "SECRET KEY: $R2_SECRET_KEY"
 
 # JSON_PAYLOAD='{
 #     "name": "'"${TOKEN_NAME}"'",
