b0bd82e6a316c2cbe81616001011aab45bed2d35
Monius <m1105930979@gmail.com>
2025-07-03 09:42:45 +0800

    update

--- debian/create-github.sh
+++ debian/create-github.sh
@@ -71,9 +71,13 @@ JSON_PAYLOAD='{
     "status": "active"
 }'
 
-RESPONSE=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
+RESPONSE=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/pagerules" \
     -H "Authorization: Bearer $CF_TOKEN" \
     -H "Content-Type: application/json" \
     -d "$JSON_PAYLOAD")
+# RESPONSE=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
+#     -H "Authorization: Bearer $CF_TOKEN" \
+#     -H "Content-Type: application/json" \
+#     -d "$JSON_PAYLOAD")
 
 echo "$RESPONSE"
