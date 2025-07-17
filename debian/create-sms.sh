ff9d94c65a5b0794f2807f360bba337c06dec50e
Monius <m1105930979@gmail.com>
2025-07-17 12:10:54 +0800

    update

--- debian/create-sms.sh
+++ debian/create-sms.sh
@@ -26,7 +26,13 @@ if [ -z "$VONAGE_API_KEY" ] || [ -z "$VONAGE_API_SECRET" ]; then
     exit 1
 fi
 
-RESPONSE=$(curl -fsSL -X GET "https://api.nexmo.com/verify/json?&api_key=$VONAGE_API_KEY&api_secret=$VONAGE_API_SECRET&number=$VONAGE_NUMBER&brand=$VONAGE_BRAND")
+# RESPONSE=$(curl -fsSL -X GET "https://api.nexmo.com/verify/json?&api_key=$VONAGE_API_KEY&api_secret=$VONAGE_API_SECRET&number=$VONAGE_NUMBER&brand=$VONAGE_BRAND")
+RESPONSE=$(curl -fsSL -X POST "https://api.nexmo.com/verify/json" \
+    -d "api_key=$VONAGE_API_KEY" \
+    -d "api_secret=$VONAGE_API_SECRET" \
+    -d "number=$VONAGE_NUMBER" \
+    -d "brand=$VONAGE_BRAND")
+
 REQUEST_ID=$(echo "$RESPONSE" | sed -n 's/.*"request_id":"\([^"]*\)".*/\1/p')
 STATUS=$(echo "$RESPONSE" | sed -n 's/.*"status":"\([^"]*\)".*/\1/p')
 
