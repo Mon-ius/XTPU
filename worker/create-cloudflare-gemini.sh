f375b6bcb8ba04ff092c8c566d83b5644afbc860
Monius <m1105930979@gmail.com>
2025-07-18 20:38:18 +0800

    update

--- worker/create-cloudflare-gemini.sh
+++ worker/create-cloudflare-gemini.sh
@@ -6,9 +6,15 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <cloudflare_token>"
     echo ""
     echo "Example:"
-    echo "  $0 eW91ci10b2tlbg== my-service"
+    echo "  $0 eW91ci10b2tlbg=="
     exit 1
 fi
 
+_CF_TOKEN_BASE64='base64encodedtoken'
+
+CF_TOKEN_BASE64="${1:-$_CF_TOKEN_BASE64}"
 SERVICE_NAME="gemini"
 SERVICE_ENDPOINT='generativelanguage.googleapis.com'
+
+curl -fsSL https://raw.githubusercontent.com/Mon-ius/XTPU/refs/heads/main/worker/create-cloudflare-proxy.sh | sh -s -- "$CF_TOKEN_BASE64" "$SERVICE_NAME" "$SERVICE_ENDPOINT"
+
