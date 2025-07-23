b4ea5ce54a6659612a0a10716426d568c6ebbfc4
Monius <m1105930979@gmail.com>
2025-07-23 20:21:33 +0800

    update

--- GCP/create-ssh-tun.sh
+++ GCP/create-ssh-tun.sh
@@ -1,12 +1,13 @@
 #!/bin/dash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <username>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <username> <pem>"
     exit 1
 fi
 
 RUSER=$1
+PEM=$2
 sudo adduser --disabled-password --disabled-login --gecos "" "$RUSER"
 sudo mkdir -p "/home/$RUSER/.ssh"
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYDGKmE+0M4PcMH/kTupjmcf/Ic9VbvXKD7fPHNVdem' | sudo tee "/home/$RUSER/.ssh/authorized_keys"
+echo "$PEM" | sudo tee "/home/$RUSER/.ssh/authorized_keys"
 sudo chown -R "$RUSER:$RUSER" "/home/$RUSER/.ssh"
