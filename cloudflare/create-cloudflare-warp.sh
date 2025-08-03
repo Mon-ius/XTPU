2656c78aba3cc9e18a8a597c8d097dc0853517db
Monius <m1105930979@gmail.com>
2025-08-04 01:56:52 +0800

    update

--- cloudflare/create-cloudflare-warp.sh
+++ cloudflare/create-cloudflare-warp.sh
@@ -22,18 +22,18 @@ RESPONSE=$(curl -s -X POST "$CF_WARP_BASE" \
     -d "$WARP_PAYLOAD")
 
 CF_ACCOUNT_ID=$(echo "$RESPONSE" | grep -o '"id":"[^"]*' | cut -d'"' -f4 | head -n 1)
-CF_CLIENT_ID=$(echo "$RESPONSE" | grep -o '"client_id":"[^"]*' | cut -d'"' -f4 | head -n 1)
 CF_TOKEN_ID=$(echo "$RESPONSE" | grep -o '"token":"[^"]*' | cut -d'"' -f4 | head -n 1)
 CF_LICENSE=$(echo "$RESPONSE" | grep -o '"license":"[^"]*' | cut -d'"' -f4 | head -n 1)
+CF_CLIENT_ID=$(echo "$RESPONSE" | grep -o '"client_id":"[^"]*' | cut -d'"' -f4 | head -n 1)
 CF_PUBLIC_KEY=$(echo "$RESPONSE" | grep -o '"public_key":"[^"]*' | cut -d'"' -f4 | head -n 1)
 CF_ADDR_V4=$(echo "$RESPONSE" | grep -o '"v4":"[^"]*' | cut -d'"' -f4 | tail -n 1)
 CF_ADDR_V6=$(echo "$RESPONSE" | grep -o '"v6":"[^"]*' | cut -d'"' -f4 | tail -n 1)
 
 WARP_RESPONSE='{
     "account":"'"$CF_ACCOUNT_ID"'",
+    "token":"'"$CF_TOKEN_ID"'",
     "license":"'"$CF_LICENSE"'",
     "client":"'"$CF_CLIENT_ID"'",
-    "token":"'"$CF_TOKEN_ID"'",
     "v4":"'"$CF_ADDR_V4"'",
     "v6":"'"$CF_ADDR_V6"'",
     "key":"'"$CF_PUBLIC_KEY"'",
