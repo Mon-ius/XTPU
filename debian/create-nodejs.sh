e31c5c94bc6dacc4ef94103128c1ea209c8133a5
Monius <m1105930979@gmail.com>
2025-08-08 03:25:27 +0800

    update

--- debian/create-nodejs.sh
+++ debian/create-nodejs.sh
@@ -14,4 +14,4 @@ curl -fsSL "$NODEJS" | sudo -E gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/nod
 echo "deb [arch=$ARCH] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo -E tee /etc/apt/sources.list.d/node_$NODE_VERSION.list
 
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y nodejs npm
+sudo -E apt-get -qq install -y nodejs
