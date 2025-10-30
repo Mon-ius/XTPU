99655e01b1b2b3a88024fb4a055fb15abf343454
Monius <m1105930979@gmail.com>
2025-10-30 09:21:49 +0800

    update

--- debian/create-dev-user.sh
+++ debian/create-dev-user.sh
@@ -10,10 +10,10 @@ fi
 
 sudo -E apt-get -qq install -y vim curl tmux
 sudo -E apt-get -qq install -o 'Dpkg::Options::=--force-confdef' -o 'Dpkg::Options::=--force-confmiss' -y \
-    net-tools zsh locales htop git git-lfs openssl gnupg2 dnsutils tree wget bzip2 unzip proxychains4
+    net-tools zsh locales htop git git-lfs openssl gnupg2 dnsutils tree wget bzip2 unzip proxychains4 gnutls-bin
 
-git config --global http.postBuffer 524288000
-git config --global https.postBuffer 524288000
+git config --global http.postBuffer 1048576000
+git config --global https.postBuffer 1048576000
 
 #---
 
