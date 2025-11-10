0381a7e2b118f308b4a10ea23fde5395f700f47a
Monius <m1105930979@gmail.com>
2025-11-10 16:55:10 +0800

    update

--- debian/create-reverse-ssh.sh
+++ debian/create-reverse-ssh.sh
@@ -3,40 +3,43 @@
 set +e
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <remote> <port>"
+    echo "Usage: $0 <addr> <port> <service>"
     echo "Example: $0 127.0.0.1 60022"
     exit 1
 fi
 
 _RHOST=127.0.0.1
 _RPORT=60022
-SNAME=reverse-ssh
+_RP=60022
+_SNAME=tun
 
 RHOST="${1:-$_RHOST}"
 RPORT="${2:-$_RPORT}"
-SECRET=/home/$USER/.ssh/id_ed25519
+SNAME="${3:-$_SNAME}"
+SECRET=/etc/ssl/certs/$SNAME
+
+IPV4=$(dig +short A "$RHOST" | head -n1)
 
 echo "[Unit]
 Description=$SNAME service
 After=network.target
 Wants=network-online.target
 StartLimitInterval=200
-StartLimitBurst=1
+StartLimitBurst=5
 
 [Service]
 Type=simple
-User=$USER
 StandardOutput=null
 StandardError=null
-ExecStart=ssh -i $SECRET -NC -o GatewayPorts=true -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -R $RPORT:127.0.0.1:22 $USER@$RHOST
-ExecStopPost=/usr/bin/ssh-keygen -R $RHOST
+ExecStart=ssh -i $SECRET -NC -o GatewayPorts=true -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -R $RPORT:127.0.0.1:22 $SNAME@$RHOST
+ExecStopPost=/usr/bin/ssh-keygen -R $IPV4
 ExecStop=/bin/true
 SuccessExitStatus=0 1
 Restart=on-failure
 RestartSec=300
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/$SNAME.service
+WantedBy=multi-user.target" | sudo tee "/etc/systemd/system/$SNAME.service"
 
 echo "[Unit]
 Description=Timer for the $SNAME service
