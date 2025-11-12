5f90970a00f8934da32da3adae8238ba7578e885
Monius <m1105930979@gmail.com>
2025-11-12 17:00:08 +0800

    update

--- debian/create-rust.sh
+++ debian/create-rust.sh
@@ -1,11 +1,19 @@
 #!/bin/dash
 
-export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y cmake
-
-curl -fsSL https://sh.rustup.rs | sh -s -- -y && . "$HOME/.cargo/env"
+# Detect OS
+if [ "$(uname)" = "Darwin" ]; then
+    brew install cmake rustup
+elif [ "$(uname)" = "Linux" ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo -E apt-get -qq update
+    sudo -E apt-get -qq install -o Dpkg::Options::="--force-confold" -y cmake
+    curl -fsSL https://sh.rustup.rs | sh -s -- -y && . "$HOME/.cargo/env"
+else
+    echo "Unsupported operating system: $(uname)"
+    exit 1
+fi
 rustup update nightly && rustup default nightly
+rustup-init -y
 
 # cargo install oha
 # host=http://127.0.0.1:10086/v1/ed25519.pub
