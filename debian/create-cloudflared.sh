b3c14b4c3daaf66ed00a9cbb85e5b2c1d0562d36
Monius <m1105930979@gmail.com>
2025-11-12 18:03:05 +0800

    update

--- debian/create-cloudflared.sh
+++ debian/create-cloudflared.sh
@@ -7,5 +7,5 @@ VER=$(lsb_release -cs)
 ARCH=$(dpkg --print-architecture)
 
 curl -fsSL "$ARGO" | sudo gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/argo.gpg
-echo "deb [arch=$ARCH] https://pkg.cloudflare.com/cloudflared $VER main" | sudo tee /etc/apt/sources.list.d/argo.list
+echo "deb [arch=$ARCH] https://pkg.cloudflare.com/cloudflared any main" | sudo tee /etc/apt/sources.list.d/argo.list
 sudo apt-get update && sudo apt-get install cloudflared
